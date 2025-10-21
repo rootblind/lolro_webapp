c6eb607386e8bcff49aa52df728637d76510ee7c
rootblind <blindcatalin@protonmail.com>
2025-10-21 22:34:30 +0300

    deployment ready configuration

--- backend/source/server.js
+++ backend/source/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const {config} = require("dotenv");
+const path = require("path")
 config();
 
 const {testRounter} = require("./routes/testRoutes.js");
@@ -14,13 +15,17 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const HOST = process.env.HOST;
 const FRONT_PORT = process.env.FRONT_PORT || 5173;
+const dirname = __dirname;
 
 // middleware
-app.use(
-    cors({
-        origin: `${HOST}:${FRONT_PORT}`
-    })
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: `${HOST}:${FRONT_PORT}`
+        })
+    );
+}
+
 app.use(express.json()); // parse json
 app.use(rateLimiter);
 
@@ -29,6 +34,15 @@ app.use(rateLimiter);
 // routes
 app.use("/test", testRounter);
 app.use("/x", xRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(dirname, "../../frontend/dist")));
+
+    app.use((req, res) => {
+        res.sendFile(path.join(dirname, "../../frontend/dist/index.html"));
+    });
+
+}
 
 // doing database tables checks and then starting the server
 (
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/test" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/test"
+    baseURL: BASE_URL
 });
 
 export default api;
